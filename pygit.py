9577212cfd1de34c79d88f8147d41e730d7899e1
Kunj <kunjthakore@gmail.com>
2022-04-11 09:54:42 +0530

    "commit"

--- pygit.py
+++ pygit.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import time
 
@@ -10,13 +11,18 @@ def git_do(cmd):
 # print("Git Init is Running")
 # git_do(['init'])
 # time.sleep(1)
-
+dict="/Users/kunjthakor/Documents/Python_Program/New_Folder"
 # print("Git Init is Running")
 
 # git_do([ 'remote', 'add', 'origin' ,'https://github.com/kunjthakore/repo2.git'])
-git_do(['add','-A'])
-git_do(['commit' ,'-m', '"commit"'])
-git_do(['push', '-u', 'origin', 'main'])
+# git_do(['add','-A'])
+# git_do(['commit' ,'-m', '"commit"'])
+# git_do(['push', '-u', 'origin', 'main'])
 
 
-
+for name in  dict:
+    if os.path.isdir(os.path.join(dict,name)):
+        git_do(['remote', 'set-url', 'origin' ,'https://github.com/kunjthakore/{name}.git'])
+        git_do(['add','-A'])
+        git_do(['commit' ,'-m', '"commit"'])
+        git_do(['push', '-u', 'origin', 'main'])
